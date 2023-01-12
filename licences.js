3cb8b42714474afe6f81914bcd2f4825bc93d39d
N.S.Devaraj <nsdevaraj@gmail.com>
2023-01-12 18:45:20 +0530

    Update licences.js

--- licences.js
+++ licences.js
@@ -3,10 +3,6 @@
 
 window.matrixPlans = [
     {
-        value: "inforiver-plan.premium-viewer",
-        label: "Premium Viewer Only"
-    },
-    {
         value: "inforiver-plan.premium",
         label: "Premium Developer"
     },
@@ -15,41 +11,69 @@ window.matrixPlans = [
         label: "Premium Unlimited Viewer and Developer"
     },
     {
-        value: "inforiver-plan.premium-embedded",
+        value: "xviz.inforiver-premium.premium-embedded",
         label: "Premium Embedded"
     },
     {
-        value: "inforiver-plan.premium-reportserver",
+        value: "xviz.inforiver-premium.premium-reportserver",
         label: "Premium Report Server"
+    },
+    {
+        value: "xviz.inforiver-premium.private",
+        label: "Premium Private Plan"
     },
     {
         value: "inforiver-plan.enterprise-viewer",
         label: "Enterprise Viewer Only"
     },
     {
-        value: "inforiver-plan.enterprise",
+        value: "xviz.inforiver-enterprise.tier2-enterprise",
         label: "Enterprise Developer"
     },
     {
-        value: "inforiver-plan.enterprise-xxviewer",
+        value: "xviz.inforiver-enterprise.enterprise-xxviewer",
         label: "Enterprise Unlimited Viewer and Developer"
     },
     {
-        value: "inforiver-plan.enterprise-embedded",
+        value: "xviz.inforiver-enterprise.enterprise-embedded",
         label: "Enterprise Embedded"
     },
     {
-        value: "inforiver-plan.enterprise-reportserver",
+        value: "xviz.inforiver-enterprise.enterprise-reportserver",
         label: "Enterprise Report Server"
     }];
 
 
-window.chartPlans = [{
-    value: "inforiver-plan.viewer",
-    label: "Viewer Only"
-}];
-
-
+window.chartPlans = [
+    {
+        value: "xviz.inforiver-charts.charts-xxviewer",
+        label: "Unlimited Viewer and Developer",
+    }, {
+        value: "xviz.inforiver-charts.inforiver-smb",
+        label: "Small Medium",
+    }, {
+        value: "xviz.inforiver-charts.inforiver-large",
+        label: "Large",
+    }, {
+        value: "xviz.inforiver-charts.inforiver_charts_5",
+        label: "Charts",
+    }, {
+        value: "xviz.inforiver-charts.private",
+        label: "Private Plan",
+    }, {
+        value: "xviz.inforiver-charts.inforiver_charts_discounted_1",
+        label: "Discounted Plan",
+    },
+    {
+        value: "xviz.inforiver-charts.charts-embedded",
+        label: "Embedded Plan",
+    },
+    {
+        value: "xviz.inforiver-charts.charts-report-server",
+        label: "Report Server Plan",
+    }
+];
+ 
 
 window.projects = [{
     value: 'inforiverMatrix',
