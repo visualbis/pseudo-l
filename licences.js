28ceb00f0fd415ddd9b498724c4eb820e2068280
praveenn77 <npu@live.in>
2023-01-12 16:47:34 +0530

    fix try

---
+++ licences.js
@@ -0,0 +1,42 @@
+
+window.matrixPlans = [
+    {
+        value: "inforiver-plan.premium-viewer",
+        label: "Premium Viewer Only"
+    },
+    {
+        value: "inforiver-plan.premium",
+        label: "Premium Developer"
+    },
+    {
+        value: "inforiver-plan.premium-xxviewer",
+        label: "Premium Unlimited Viewer and Developer"
+    },
+    {
+        value: "inforiver-plan.premium-embedded",
+        label: "Premium Embedded"
+    },
+    {
+        value: "inforiver-plan.premium-reportserver",
+        label: "Premium Report Server"
+    },
+    {
+        value: "inforiver-plan.enterprise-viewer",
+        label: "Enterprise Viewer Only"
+    },
+    {
+        value: "inforiver-plan.enterprise",
+        label: "Enterprise Developer"
+    },
+    {
+        value: "inforiver-plan.enterprise-xxviewer",
+        label: "Enterprise Unlimited Viewer and Developer"
+    },
+    {
+        value: "inforiver-plan.enterprise-embedded",
+        label: "Enterprise Embedded"
+    },
+    {
+        value: "inforiver-plan.enterprise-reportserver",
+        label: "Enterprise Report Server"
+    }];
