b22249f6d117b3704c3840c0d476e87bffa309af
manikandanvengatesan <manikandan.vengatesan@gmail.com>
2023-03-28 15:49:09 +0530

    feat(xviz): added license for xviz chart

--- licences.js
+++ licences.js
@@ -1,75 +1,121 @@
-
-
-
 window.matrixPlans = [
-    {
-        value: "xviz.inforiver-premium.tier1-premium",
-        label: "Premium Matrix 1(1 tp 19 users)"
-    },
-        {
-        value: "xviz.inforiver-premium.tier3-premium",
-        label: "Premium Matrix 3(100 to 999 users)"
-    },
-        {
-        value: "xviz.inforiver-premium.tier4-premium",
-        label: "Premium Matrix 4(1000 to 1999 users)"
-    },
-    {
-        value: "xviz.inforiver-premium.premium-xxviewer",
-        label: "Premium Developer - Unlimited Viewers"
-    },
-    {
-        value: "xviz.inforiver-premium.private",
-        label: "Premium Private Plan"
-    },
-    {
-        value: "xviz.inforiver-enterprise.tier2-enterprise",
-        label: "Enterprise Matrix SAAS Edition"
-    },
-    {
-        value: "xviz.inforiver-enterprise.enterprise-xxviewer",
-        label: "Enterprise Matrix Developer - Unlimited Viewers"
-    }];
-
+  {
+    value: "xviz.inforiver-premium.tier1-premium",
+    label: "Premium Matrix 1(1 tp 19 users)",
+  },
+  {
+    value: "xviz.inforiver-premium.tier3-premium",
+    label: "Premium Matrix 3(100 to 999 users)",
+  },
+  {
+    value: "xviz.inforiver-premium.tier4-premium",
+    label: "Premium Matrix 4(1000 to 1999 users)",
+  },
+  {
+    value: "xviz.inforiver-premium.premium-xxviewer",
+    label: "Premium Developer - Unlimited Viewers",
+  },
+  {
+    value: "xviz.inforiver-premium.private",
+    label: "Premium Private Plan",
+  },
+  {
+    value: "xviz.inforiver-enterprise.tier2-enterprise",
+    label: "Enterprise Matrix SAAS Edition",
+  },
+  {
+    value: "xviz.inforiver-enterprise.enterprise-xxviewer",
+    label: "Enterprise Matrix Developer - Unlimited Viewers",
+  },
+];
 
 window.chartPlans = [
-    {
-        value: "xviz.inforiver-charts.charts-xxviewer",
-        label: "Unlimited Viewer and Developer",
-    }, {
-        value: "xviz.inforiver-charts.inforiver-smb",
-        label: "Small Medium",
-    }, {
-        value: "xviz.inforiver-charts.inforiver-large",
-        label: "Large",
-    }, {
-        value: "xviz.inforiver-charts.inforiver_charts_5",
-        label: "Charts",
-    }, {
-        value: "xviz.inforiver-charts.private",
-        label: "Private Plan",
-    }, {
-        value: "xviz.inforiver-charts.inforiver_charts_discounted_1",
-        label: "Discounted Plan",
-    },
-    {
-        value: "xviz.inforiver-charts.charts-embedded",
-        label: "Embedded Plan",
-    }
+  {
+    value: "xviz.inforiver-charts.charts-xxviewer",
+    label: "Unlimited Viewer and Developer",
+  },
+  {
+    value: "xviz.inforiver-charts.inforiver-smb",
+    label: "Small Medium",
+  },
+  {
+    value: "xviz.inforiver-charts.inforiver-large",
+    label: "Large",
+  },
+  {
+    value: "xviz.inforiver-charts.inforiver_charts_5",
+    label: "Charts",
+  },
+  {
+    value: "xviz.inforiver-charts.private",
+    label: "Private Plan",
+  },
+  {
+    value: "xviz.inforiver-charts.inforiver_charts_discounted_1",
+    label: "Discounted Plan",
+  },
+  {
+    value: "xviz.inforiver-charts.charts-embedded",
+    label: "Embedded Plan",
+  },
 ];
- 
 
-window.projects = [{
-    value: 'inforiverMatrix',
-    label: 'Inforiver Matrix'
-},
-{
-    value: 'inforiverCharts',
-    label: 'Inforiver Charts'
-}];
+window.xvizPlans = [
+  {
+    value: "xviz.multiple-axes-chart-0",
+    label: "$0 private plan",
+  },
+  {
+    value: "xviz.multiple-axes-chart-1",
+    label: "Per user plan, with a trial",
+  },
+  {
+    value: "xviz.multiple-axes-chart-2",
+    label: "Per user plan 99",
+  },
+  {
+    value: "xviz.multiple-axes-chart-3",
+    label: "Per user plan 999",
+  },
+  {
+    value: "xviz.multiple-axes-chart-4",
+    label: "Per user plan 9999",
+  },
+  {
+    value: "xviz.multiple-axes-chart-5",
+    label: "Per user plan 99999",
+  },
+  {
+    value: "xviz.multiple-axes-chart-6",
+    label: "Per user plan Min 99999",
+  },
+  {
+    value: "xviz.multiple-axes-chart-embedded",
+    label: "$0 Embedded",
+  },
+  {
+    value: "xviz.multiple-axes-chart-report-server",
+    label: "$0 Report Server",
+  },
+];
 
+window.projects = [
+  {
+    value: "inforiverMatrix",
+    label: "Inforiver Matrix",
+  },
+  {
+    value: "inforiverCharts",
+    label: "Inforiver Charts",
+  },
+  {
+    value: "xviz",
+    label: "Xviz Charts",
+  },
+];
 
 window.projectPlans = {
-    inforiverMatrix: window.matrixPlans,
-    inforiverCharts: window.chartPlans
-}
+  inforiverMatrix: window.matrixPlans,
+  inforiverCharts: window.chartPlans,
+  xviz: window.xvizPlans,
+};
